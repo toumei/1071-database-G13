23a7af7e78a90850f74c61e1707193dfc7d07d60
prunus520 <prunus520@gmail.com>
2018-12-05 00:56:11 +0800

    dbCtrl table

--- Express/routes/index.js
+++ Express/routes/index.js
@@ -2,8 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    res.redirect('/index');
+});
+
+router.get('/index', function (req, res, next) {
+    res.render('index', { title: '首頁' });
+
 });
 
 module.exports = router;
