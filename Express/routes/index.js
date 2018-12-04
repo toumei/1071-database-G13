1902cf72fe0772b1eb3dadb4199cf28e4cfc9a87
prunus520 <prunus520@gmail.com>
2018-12-04 09:11:50 +0800

    update

--- Express/routes/index.js
+++ Express/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'index' });
+  res.render('index', { title: '首頁' });
 });
 
 module.exports = router;
