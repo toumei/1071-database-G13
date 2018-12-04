664518e5be011a42a7cdd6e72154adf63d1a433d
prunus520 <prunus520@gmail.com>
2018-12-04 18:09:18 +0800

    table

--- Express/routes/index.js
+++ Express/routes/index.js
@@ -1,14 +1,10 @@
 var express = require('express');
 var router = express.Router();
-const productsModel = require('../models/products_model');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-    productsModel.fetchAll()
-        .then(([data]) => {
-            res.render('index', { title: '首頁', data: data });
-        })
-        .catch(err => console.log(err));
+    res.render('index', { title: '首頁' });
+
 });
 
 module.exports = router;
