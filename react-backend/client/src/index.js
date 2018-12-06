4042ba968ac902ba8a4757a7303d678c0bda192a
prunus520 <prunus520@gmail.com>
2018-12-06 19:30:36 +0800

    client update

--- react-backend/client/src/index.js
+++ react-backend/client/src/index.js
@@ -2,60 +2,35 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Navbar from './partials/Navbar';
 import Header from './partials/Header';
-// import App from './App';
+import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { Router, Route, Link, hashHistory } from 'react-router'
+// import { BrowserRouter, HashRouter, Route } from 'react-router-dom'
 
 ReactDOM.render(<Navbar />, document.getElementById('navbar'));
 ReactDOM.render(<Header />, document.getElementById('header'));
-// ReactDOM.render(<App />, document.getElementById('root'));
-class App extends React.Component {
-    render() {
-        return (
-            <div>
-                <h1>App</h1>
-                <ul>
-                    <li><Link to="/about">About</Link></li>
-                    <li><Link to="/users/1">Users 1</Link></li>
-                    <li><Link to="/users/2">Users 2</Link></li>
-                </ul>
-                {this.props.children}
-            </div>
-        );
-    }
-}
+ReactDOM.render(<App />, document.getElementById('root'));
 
-class About extends React.Component {
-    render() {
-        return (<h1>About</h1>);
-    }
-}
+// function Home() {
+//     return <h2>Home Page</h2>
+// }
 
-class Users extends React.Component {
-    render() {
-        const id = this.props.params.userId;
-        return (
-            <div>
-                user id: {id}
-            </div>
-        )
-    }
-}
+// function Second() {
+//     return <h2>a Page</h2>
+// }
 
+// function Third() {
+//     return <h2>b Page</h2>
+// }
 
-ReactDOM.render(
-    (
-        <Router history={hashHistory}>
-            <Route path="/" component={App}>
-                <Route path="about" component={About} />
-                <Route path="users/:userId" component={Users} />
-            </Route>
-        </Router>
-    ),
-    document.getElementById('root')
-);
+// ReactDOM.render(
+//     (
+//         <BrowserRouter>
+//             <Route path='/' component={Home}></Route>
+//             {/* <Route path='/second' component={Second}></Route>
+//             <Route path='/third' component={Third}></Route> */}
+//         </BrowserRouter>
+//     ),
+//     document.getElementById('root')
+// );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
 serviceWorker.unregister();
