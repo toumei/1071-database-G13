7a7500a8e89ce6854d31ded87239d1f6982ff15c
toumei <ricknbmnwo@gmail.com>
2018-12-06 19:00:52 +0800

    mv client & server -> react-backend

--- server/routes/yourid.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-router.get('/:yourid', function(req, res, next) {
-
-    res.render('yourID', { id: req.params.yourid});
-});
-module.exports = router;
