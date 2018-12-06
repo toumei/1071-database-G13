05968c40559d613582903f8a3ae783dec9957fe7
prunus520 <prunus520@gmail.com>
2018-12-06 20:43:31 +0800

    product 錯誤更正

--- react-backend/server/models/products_model.js
+++ react-backend/server/models/products_model.js
@@ -1,16 +1,16 @@
 const db = require('../config/mysql');
 const $sql = require('./mapper').product;
 
-module.exports = class Product{
-    constructor(id, name, price){
+module.exports = class Product {
+    constructor(id, name, price) {
         this.id = id;
         this.name = name;
         this.price = price;
     }
 
-    static insert(sqlData){ return db.query($sql.insert, sqlData); };
-    static update(sqlData, id){ return db.query($sql.update, [sqlData, id]);};
-    static delete(id){return db.execute($sql.delete, [id]);};
-    static fetchAll(){ return db.execute($sql.fetchAll); };
-    static fetchById(id){ return db.execute($sql.fetchById, [id]); };
+    static insert(sqlData) { return db.query($sql.insert, sqlData); };
+    static update(sqlData, id) { return db.query($sql.update, [sqlData, id]); };
+    static delete(id) { return db.execute($sql.delete, [id]); };
+    static fetchAll() { return db.execute($sql.fetchAll); };
+    static fetchById(id) { return db.execute($sql.fetchById, [id]); };
 };
