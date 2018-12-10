2894a2d1f39143a6c02e2925158520825e3c549e
prunus520 <37388924+prunus520@users.noreply.github.com>
2018-12-10 19:04:29 +0800

    Add files via upload

---
+++ react-backend/server/config/database.js
@@ -0,0 +1,19 @@
+module.exports = {
+  mysql: {
+    host: "localhost",
+    user: "root",
+    password: "0000",
+    database: "dbdemo",
+    port: 3306,
+    connectionLimit: 10
+  },
+  mysql2: {
+    host: "localhost",
+    user: "root",
+    password: "0000",
+    database: "res_net_cmms",
+    port: 3306,
+    connectionLimit: 10,
+    multipleStatements: true
+  }
+};
