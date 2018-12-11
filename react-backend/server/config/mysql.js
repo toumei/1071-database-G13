8a8c359b160086ce748690c8039e91c78170fcb1
prunus520 <prunus520@gmail.com>
2018-12-11 19:45:21 +0800

    delete config

--- react-backend/server/config/mysql.js
+++
@@ -1,3 +0,0 @@
-var mysql = require("mysql2");
-var dbConfig = require("./database").mysql;
-module.exports = mysql.createPool(dbConfig).promise();
--- react-backend/server/config/mysql2.js
+++
@@ -1,3 +0,0 @@
-var mysql = require("mysql2");
-var dbConfig = require("./database").mysql2;
-module.exports = mysql.createPool(dbConfig).promise();
