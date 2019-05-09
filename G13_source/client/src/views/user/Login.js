2a6d4e18480ffdc114ad4c23f9c146402e1caab0
prunus520 <prunus520@gmail.com>
2019-05-09 13:47:34 +0800

    bug fix

--- G13_source/client/src/views/user/Login.js
+++ G13_source/client/src/views/user/Login.js
@@ -31,31 +31,46 @@ export default class extends Component {
   }
   render() {
     return (
-      <div className="container">
+      <div className="container" style={{ marginTop: 10 }}>
         <form name="addform" id="addform" onSubmit={this.handleSubmit}>
-          <label>ID</label>
-          <input
-            type="text"
-            className="form-control"
-            id="idInput"
-            name="id"
-            placeholder="id"
-            ref="idInput"
-            onChange={this.handleChange}
-          />
-          <label>PWD</label>
-          <input
-            type="text"
-            className="form-control"
-            id="passwordInput"
-            name="password"
-            placeholder="password"
-            ref="passwordInput"
-            onChange={this.handleChange}
-          />
-          <button type="submit" className="btn btn-primary">
-            Submit
-          </button>
+          <div className="form-group row">
+            <label className="col-md-1" htmlFor="idInput">
+              學號
+            </label>
+            <div className="input-group col-md-11">
+              <input
+                type="text"
+                className="form-control"
+                id="idInput"
+                name="id"
+                placeholder="學號"
+                ref="idInput"
+                onChange={this.handleChange}
+              />
+            </div>
+          </div>
+          <div className="form-group row">
+            <label className="col-md-1" htmlFor="passwordInput">
+              密碼
+            </label>
+            <div className="input-group col-md-11">
+              <input
+                type="text"
+                className="form-control"
+                id="passwordInput"
+                name="password"
+                placeholder="密碼"
+                ref="passwordInput"
+                onChange={this.handleChange}
+              />
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-11" />
+            <button type="submit" className="btn btn-primary">
+              登入
+            </button>
+          </div>
         </form>
       </div>
     );
